f4bde2cce4ff917b99f11e7fc7f3facb7c03878a
Kagan Girgin <kagan@futureworksmobile.com>
2024-05-09 23:40:28 +0300

    version change

--- Package.swift
+++ Package.swift
@@ -1,28 +1,35 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "Unrealm",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "UnrealmObjC", targets: ["UnrealmObjC"]),
         .library(name: "Unrealm", targets: ["Unrealm", "UnrealmObjC"])
     ],
     dependencies: [
-        .package(url: "https://github.com/realm/realm-swift.git", from: "10.39.0"),
+        .package(url: "https://github.com/realm/realm-swift.git", exact: "10.49.1"),
         .package(url: "https://github.com/wickwirew/Runtime.git", from: "2.2.2")
     ],
     targets: [
         .target(
             name: "UnrealmObjC",
-            dependencies: ["Realm", "RealmSwift"],
+            dependencies: [
+                .product(name: "Realm", package: "realm-swift"),
+                .product(name: "RealmSwift", package: "realm-swift"),
+            ],
             path: "Unrealm/Classes/ObjC"
         ),
         .target(
             name: "Unrealm",
-            dependencies: ["UnrealmObjC", "Realm", "RealmSwift", "Runtime"],
+            dependencies: [
+                .product(name: "Realm", package: "realm-swift"),
+                .product(name: "RealmSwift", package: "realm-swift"),
+                "UnrealmObjC", "Runtime"
+            ],
             path: "Unrealm/Classes/Swift"
         )
     ],
